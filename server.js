c126163b3c58018baf60a999d24aa428e789dadf
Ivan Timofeev <Saskeu933@gmail.com>
2026-06-05 21:01:00 +0300

    Add routes

--- server.js
+++ server.js
@@ -6,6 +6,40 @@ app.get('/', (req, res) => {
     res.send('<h1>Привет, Октагон!</h1>');
 });
 
+app.get('/static', (req, res) => {
+    res.json({
+        header: 'Hello',
+        body: 'Octagon NodeJS Test'
+    });
+});
+
+app.get('/dynamic', (req, res) => {
+
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+    const c = Number(req.query.c);
+
+    if (
+        req.query.a === undefined ||
+        req.query.b === undefined ||
+        req.query.c === undefined ||
+        isNaN(a) ||
+        isNaN(b) ||
+        isNaN(c)
+    ) {
+        return res.json({
+            header: 'Error'
+        });
+    }
+
+    const result = (a * b * c) / 3;
+
+    res.json({
+        header: 'Calculated',
+        body: String(result)
+    });
+});
+
 app.listen(3000, () => {
     console.log('Сервер запущен на порту 3000');
 });
