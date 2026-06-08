878fb78b2e5e90d178bc63435b1dde4f5710f357
Ivan Timofeev <Saskeu933@gmail.com>
2026-06-09 02:25:19 +0300

    Add MySQL CRUD

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 
 const app = express();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ChatBotTests'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log('Ошибка подключения');
+        return;
+    }
+
+    console.log('MySQL подключен');
+});
 
 app.get('/', (req, res) => {
     res.send('<h1>Привет, Октагон!</h1>');
@@ -39,7 +56,130 @@ app.get('/dynamic', (req, res) => {
         body: String(result)
     });
 });
+app.get('/getAllItems', (req, res) => {
 
+    connection.query(
+        'SELECT * FROM Items',
+        (err, results) => {
+
+            if (err) {
+                console.log(err);
+                return res.json(null);
+            }
+
+            res.json(results);
+        }
+    );
+
+});
+app.get('/addItem', (req, res) => {
+
+    const name = req.query.name;
+    const desc = req.query.desc;
+
+    if (
+        name === undefined ||
+        desc === undefined
+    ) {
+        return res.json(null);
+    }
+
+    connection.query(
+        'INSERT INTO Items(name, `desc`) VALUES(?, ?)',
+        [name, desc],
+        (err, result) => {
+
+            if (err) {
+                console.log(err);
+                return res.json(null);
+            }
+
+            res.json({
+                id: result.insertId,
+                name: name,
+                desc: desc
+            });
+
+        }
+    );
+
+});
+app.get('/deleteItem', (req, res) => {
+
+    const id = Number(req.query.id);
+
+    if (
+        req.query.id === undefined ||
+        isNaN(id)
+    ) {
+        if (req.query.id === undefined) {
+    return res.json({
+        error: 'Missing id'
+    });
+}
+    }
+
+    connection.query(
+        'DELETE FROM Items WHERE id = ?',
+        [id],
+        (err, result) => {
+
+            if (err) {
+                console.log(err);
+                return res.json({
+    error: 'Parameters name and desc are required'
+});
+            }
+
+            res.json({
+                deleted: result.affectedRows > 0
+            });
+        }
+    );
+
+});
+
+app.get('/updateItem', (req, res) => {
+
+    const id = Number(req.query.id);
+    const name = req.query.name;
+    const desc = req.query.desc;
+
+    if (
+        req.query.id === undefined ||
+        name === undefined ||
+        desc === undefined ||
+        isNaN(id)
+    ) {
+        if (
+    req.query.id === undefined ||
+    name === undefined ||
+    desc === undefined ||
+    isNaN(id)
+) {
+    return res.json({
+        error: 'Missing parameters'
+    });
+}
+    }
+
+    connection.query(
+        'UPDATE Items SET name = ?, `desc` = ? WHERE id = ?',
+        [name, desc, id],
+        (err, result) => {
+
+            if (err) {
+                console.log(err);
+                return res.json(null);
+            }
+
+            res.json({
+                updated: result.affectedRows > 0
+            });
+        }
+    );
+
+});
 app.listen(3000, () => {
     console.log('Сервер запущен на порту 3000');
 });
